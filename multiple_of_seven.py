552fd85d6163168a303dda6630bff112b63eef8f
Surabhi-M-R <surabhimr27@gmail.com>
2025-04-12 23:37:30 +0530

    python

---
+++ multiple_of_seven.py
@@ -0,0 +1,5 @@
+num=int(input(" enter a number"))
+if num%7==0:
+    print(" multiple of seven")
+else:
+    print(" not a multiple of seven")
---
+++ second_program.py
@@ -0,0 +1,7 @@
+str="write  a programm to input 2 numbers and print their sum"
+print(str.endswith("sum"))
+print(str.endswith("print"))
+print(str.capitalize())
+print(str.replace("u","a"))
+
+
