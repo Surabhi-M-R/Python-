ff692e0ed59281236d267b6082371a5221b89035
Surabhi-M-R <surabhimr27@gmail.com>
2026-02-16 02:07:08 +0530

    type casting

---
+++ TypeCasting/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+print(type(is_student))
---
+++ TypeCasting/typeCasting.py
@@ -0,0 +1,13 @@
+age=21
+name="surabhi"
+cgpa=3.2
+is_student=True
+
+print(type(age))
+print(type(name))
+print(type(cgpa))
+
+
+cgpa=int(cgpa)
+print(cgpa)
+print(type(cgpa))
