63c305bc72b312039b9080560938ba56424fbf6e
Surabhi-M-R <surabhimr27@gmail.com>
2026-02-16 00:39:36 +0530

    variabels

---
+++ Variables/var.py
@@ -0,0 +1,20 @@
+first_name="surabhi"
+
+print(f"Heelo,{first_name}")
+print("hello")
+
+#Integers
+age=25
+print(f"hello everyone im {age} years old")
+
+#Float
+price=23.43
+print(f"The price of the product is {price}")
+
+#Boolean
+is_student=True 
+
+if is_student: 
+    print("yes")
+else:
+    print("Not a student")
