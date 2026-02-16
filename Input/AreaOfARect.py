15ec1fefb03336da0e2cb048b85081334f3f2757
Surabhi-M-R <surabhimr27@gmail.com>
2026-02-16 23:25:30 +0530

    finding the area of a rectangle

---
+++ Input/AreaOfARect.py
@@ -0,0 +1,6 @@
+length=float    (input("enter the lenght of the retangle  "))
+breadth=float(input("Enter the breadth of a rectangle"))
+
+area=length*breadth
+
+print(f"The area of the rectangle is {area}")
